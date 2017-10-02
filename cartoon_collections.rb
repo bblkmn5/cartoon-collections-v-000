be8fa10c35de664ad638e0882418aebbf0680abe
Ben Blackman <bblkmn5@gmail.com>
2017-10-02 23:29:36 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |veggies| veggies.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_short)
+  calls_short.any? { |calls| calls.length > 4}
 end
 
 def find_the_cheese# code an argument here
