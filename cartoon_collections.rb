1351e8b1cd7b4d9f30ec2f518703de8a117bff8a
Ben Blackman <bblkmn5@gmail.com>
2017-10-02 23:06:28 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf)
+  i = 0
+  while dwarf.length < i do |i|
+    puts "#{dwarf.each_with_index}"
+    i+=1
+  end
 end
 
 def summon_captain_planet# code an argument here
